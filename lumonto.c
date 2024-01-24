7a08a38f489f6e79e5e1d18eb6548ea2f0e2b6f1
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2024-01-24 12:41:48 -0800

    quick sorting

--- lumonto.c
+++ lumonto.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * lomuto_sorting - functio to perform quick sorting on list of integers using Lomunto partitioning
+ * lomuto_sorting - functio to perform quick sorting on
+ * list of integers using Lomunto partitioning.
  * @array: Array of integers to be sorted
  * @size: array size
  * @lowest: lowest indext in the array to be sorted
@@ -11,18 +12,18 @@
  */
 void lomuto_sorting(int *array, size_t size, int lowest, int highest)
 {
-        int partition;
+	int partition;
 
-        if (highest - lowest > 0)
-        {
-                partition = lomuto_partition(array, size, lowest, highest);
-                lomuto_sorting(array, size, lowest, partition - 1);
-                lomuto_sorting(array, size, partition + 1, highest);
-        }
+	if (highest - lowest > 0)
+	{
+		partition = lomuto_partition(array, size, lowest, highest);
+		lomuto_sorting(array, size, lowest, partition - 1);
+		lomuto_sorting(array, size, partition + 1, highest);
+	}
 }
 /**
  * lomuto_partition - Order a subset of an array of integers according to
- *                    the lomuto partition scheme (last element as pivot).
+ * the lomuto partition scheme (last element as pivot).
  * @array: The array of integers.
  * @size: The size of the array.
  * @lowest: lowest index of array to be sorted.
@@ -32,27 +33,25 @@ void lomuto_sorting(int *array, size_t size, int lowest, int highest)
  */
 int lomuto_partition(int *array, size_t size, int lowest, int highest)
 {
-        int *pivot, lower, upper;
+	int *pivot, lower, upper;
 
-        pivot = array + highest;
-        for (upper = lower = lowest; lower < highest; lower++)
-        {
-                if (array[lower] < *pivot)
-                {
-                        if (upper < lower)
-                        {
-                                swich_integers(array + lower, array + upper);
-                                print_array(array, size);
-                        }
-                        upper++;
-                }
-        }
-
-        if (array[upper] > *pivot)
-{
-swich_integers(array + upper, pivot);
-                print_array(array, size);
-        }
-
-        return (upper);
+	pivot = array + highest;
+	for (upper = lower = lowest; lower < highest; lower++)
+	{
+		if (array[lower] < *pivot)
+		{
+			if (upper < lower)
+			{
+				swich_integers(array + lower, array + upper);
+				print_array(array, size);
+			}
+			upper++;
+		}
+	}
+	if (array[upper] > *pivot)
+	{
+		swich_integers(array + upper, pivot);
+		print_array(array, size);
+	}
+	return (upper);
 }
